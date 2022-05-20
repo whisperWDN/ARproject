11dccf39242a132798b09790c4e94cf26fea17e0
昊 李 <18888911937@163.com>
2022-05-20 21:33:06 +0800

    完成了3D文字的显示

--- ARproject/ARproject/App/ContentView.swift
+++ ARproject/ARproject/App/ContentView.swift
@@ -12,28 +12,45 @@ import ARKit
 struct ContentView : View {
     @StateObject var arViewModel = ARViewModel()
     @State var addEntity = false
+    @State var add3DWord = false
     @State var EntityName = ""
     @State var intensity = CGFloat(0.0)
     @State var showingList = false
     var body: some View {
         ZStack(alignment: .bottom){
-            ARViewContainer(addEntity: $addEntity, EntityName:$EntityName,intensity: $intensity).gesture(TapGesture().onEnded(){
+            ARViewContainer(addEntity: $addEntity, EntityName:$EntityName,add3DWord:$add3DWord, intensity: $intensity).gesture(TapGesture().onEnded(){
                 
             })
             
-            if showingList{
-                AddEntityList(addEntity: $addEntity, EntityName: $EntityName,showingList:$showingList)
-            }else {
+
+            HStack{
+                if showingList{
+                    AddEntityList(addEntity: $addEntity, EntityName: $EntityName,showingList:$showingList)
+                }else {
+                    Button(action: {
+                        showingList = true
+                    }, label: {
+                        Image(systemName: "chevron.right.2")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 75, height: 75, alignment: .center)
+                            .clipShape(Circle())
+                            .opacity(0.5)
+                    })
+                }
+                
                 Button(action: {
-                    showingList = true
+                    add3DWord = true
                 }, label: {
-                    Image(systemName: "chevron.right.2")
+                    Image(systemName: "square.and.pencil")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 75, height: 75, alignment: .center)
-                        .clipShape(Circle())
+                        .foregroundColor(.white)
                         .opacity(0.5)
                 })
+                
+
             }
 
 
@@ -49,7 +66,9 @@ struct ARViewContainer: UIViewRepresentable {
     @EnvironmentObject var arViewModel :ARViewModel
     @Binding var addEntity: Bool
     @Binding var EntityName: String
+    @Binding var add3DWord :Bool
     @Binding var intensity: CGFloat
+    
     
     func makeUIView(context: Context) -> ARView {
 //        arViewModel.arView.enableTapGesture()
@@ -81,6 +100,18 @@ struct ARViewContainer: UIViewRepresentable {
             DispatchQueue.main.async {
                 addEntity = false
             }
+        }
+        
+        if add3DWord{
+            let anchorEntity = AnchorEntity(plane: .horizontal)
+            let textMesh = MeshResource.generateText("桌面设计",extrusionDepth: 0.05,font: .systemFont(ofSize: 0.2),containerFrame: CGRect(),alignment: .left,lineBreakMode: .byWordWrapping)
+            let material = SimpleMaterial(color: .red, isMetallic: true)
+            let textEntity = ModelEntity(mesh: textMesh, materials: [material])
+            textEntity.generateCollisionShapes(recursive: false)
+            arViewModel.arView.installGestures([.translation],for: textEntity)
+            
+            anchorEntity.addChild(textEntity)
+            arViewModel.arView.scene.addAnchor(anchorEntity)
         }
 
         
